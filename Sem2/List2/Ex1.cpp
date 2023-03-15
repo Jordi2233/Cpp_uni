101709428b7f0ee8e4205bd7e48c7493a2af62fa
Jordi2233 <User123.Name456@protonmail.com>
2023-03-15 09:16:37 +0100

    Finish list nr 2 Ex1 and Ex2 finished with clean code

--- Sem2/List2/Ex1.cpp
+++ Sem2/List2/Ex1.cpp
@@ -1,19 +1,66 @@
 #include <iostream>
+#include <numeric>
 
+using namespace std;
+
+// a. Zwraca sumę size pierwszych elementów tablicy tab
 int suma(const int tab[], unsigned size) {
-    int result = 0;
-    for (unsigned i = 0; i < size; i++) {
-        result += tab[i];
-    }
-    return result;
+    return accumulate(tab, tab + size, 0);
 }
 
+// b. To samo co powyżej, ale wynik przekazywany jest poprzez result
+void suma(const int tab[], unsigned size, int* result) {
+    *result = accumulate(tab, tab + size, 0);
+}
 
+// c. Wypisuje size pierwszych elementów tablicy w strumieniu out, np. w std::cout
+void pisz(const int* tab, size_t size, ostream * out) {
+    for (size_t i = 0; i < size; ++i) {
+        *out << tab[i] << " ";
+    }
+    *out << endl;
+}
+
+// d. Zapisuje w tablicy kolejne liczby całkowite, od 0 do size - 1
+void iota(int tab[], unsigned size) {
+    for (unsigned i = 0; i < size; ++i) {
+        tab[i] = i;
+    }
+}
+
+// e. Odwraca kolejność size pierwszych elementów w tablicy tab
+void reverse(int* tab, size_t size) {
+    for (size_t i = 0; i < size / 2; ++i) {
+        swap(tab[i], tab[size - i - 1]);
+    }
+}
+
+// f. Oblicza iloczyn skalarny elementów 2 tablic o rozmiarze size
+int dot_product(const int* tab1, const int* tab2, size_t size) {
+    return inner_product(tab1, tab1 + size, tab2, 0);
+}
 
 int main() {
-	system("clear");
+    // Testy dla poszczególnych funkcji
+    int tab[] = { 1, 2, 3, 4, 5 };
+    int result;
 
-	std::cout 
+    cout << "suma: " << suma(tab, 3) << endl;
+    suma(tab, 3, &result);
+    cout << "suma z wynikiem przekazywanym przez wskaźnik: " << result << endl;
 
-	return 0;
+    pisz(tab, 3, &cout);
+
+    iota(tab, 5);
+    pisz(tab, 5, &cout);
+
+    reverse(tab, 5);
+    pisz(tab, 5, &cout);
+
+    int tab1[] = { 1, 2, 3 };
+    int tab2[] = { 4, 5, 6 };
+
+    cout << "iloczyn skalarny: " << dot_product(tab1, tab2, 3) << endl;
+
+    return 0;
 }
--- Sem2/List2/Ex2.cpp
+++ Sem2/List2/Ex2.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <complex>
+
+using namespace std;
+
+// Funkcja porównująca dwa liczby zespolone
+int cmp(const void* a, const void* b) {
+    const complex<double>* c1 = static_cast<const complex<double>*>(a);
+    const complex<double>* c2 = static_cast<const complex<double>*>(b);
+    if (abs(*c1) < abs(*c2)) {
+        return -1;
+    } else if (abs(*c1) > abs(*c2)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 int main() {
-	system("clear");
+    // Inicjalizacja generatora liczb pseudolosowych
+    srand(time(NULL));
 
-	return 0;
+    // Tworzenie tablicy liczb zespolonych
+    const int N = 10;
+    complex<double> tab[N];
+    for (int i = 0; i < N; ++i) {
+        double re = rand() % 10 - 5;
+        double im = rand() % 10 - 5;
+        tab[i] = complex<double>(re, im);
+    }
+
+    // Wypisanie tablicy przed sortowaniem
+    cout << "Przed sortowaniem:" << endl;
+    for (int i = 0; i < N; ++i) {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+
+    // Sortowanie tablicy
+    qsort(tab, N, sizeof(complex<double>), cmp);
+
+    // Wypisanie tablicy po sortowaniu
+    cout << "Po sortowaniu:" << endl;
+    for (int i = 0; i < N; ++i) {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
