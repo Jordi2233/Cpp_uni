686e8053d4612147967204bb31574c359cbfc911
Jordi2233 <User123.Name456@protonmail.com>
2023-05-07 10:03:36 +0200

    Bug fix Bug fix in Ex1.cpp and finished Ex2.cpp

--- Sem2/List6/Ex1.cpp
+++ Sem2/List6/Ex1.cpp
@@ -1,60 +1,103 @@
 #include <iostream>
 
-#include <iostream>
-
 class Fraction
 {
-	// or C++17's std::gcd
-	constexpr int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+    constexpr int64_t gcd(int64_t a, int64_t b) { return b == 0 ? a : gcd(b, a % b); }
 
-	int n, d;
+    int64_t n, d;
 
 public:
-	constexpr Fraction(int n, int d = 1) : n(n / gcd(n, d)), d(d / gcd(n, d)) {}
+    constexpr Fraction(int64_t n, int64_t d = 1) : n(n / gcd(n, d)), d(d / gcd(n, d)) {}
 
-	constexpr int num() const { return n; }
-	constexpr int den() const { return d; }
+    constexpr int64_t num() const { return n; }
+    constexpr int64_t den() const { return d; }
 
-	constexpr Fraction &operator*=(const Fraction &rhs)
-	{
-		int new_n = n * rhs.n / gcd(n * rhs.n, d * rhs.d);
-		d = d * rhs.d / gcd(n * rhs.n, d * rhs.d);
-		n = new_n;
-		return *this;
-	}
+    constexpr Fraction &operator*=(const Fraction &rhs)
+    {
+        int64_t new_n = n * rhs.n / gcd(n * rhs.n, d * rhs.d);
+        d = d * rhs.d / gcd(n * rhs.n, d * rhs.d);
+        n = new_n;
+        return *this;
+    }
+
+    constexpr Fraction &operator+=(const Fraction &rhs)
+    {
+        int64_t lcm = d * rhs.d / gcd(d, rhs.d);
+        n = n * (lcm / d) + rhs.n * (lcm / rhs.d);
+        d = lcm;
+        int64_t g = gcd(n, d);
+        n /= g;
+        d /= g;
+        return *this;
+    }
+
+    constexpr Fraction &operator-=(const Fraction &rhs)
+    {
+        int64_t lcm = d * rhs.d / gcd(d, rhs.d);
+        n = n * (lcm / d) - rhs.n * (lcm / rhs.d);
+        d = lcm;
+        int64_t g = gcd(n, d);
+        n /= g;
+        d /= g;
+        return *this;
+    }
+
+    constexpr Fraction &operator/=(const Fraction &rhs)
+    {
+        n = n * rhs.d;
+        d = d * rhs.n;
+        int64_t g = gcd(n, d);
+        n /= g;
+        d /= g;
+        return *this;
+    }
+
+    constexpr double to_double() const
+    {
+        return static_cast<double>(n) / static_cast<double>(d);
+    }
 };
 
 std::ostream &operator<<(std::ostream &out, const Fraction &f)
 {
-	return out << f.num() << '/' << f.den();
+    return out << f.num() << '/' << f.den();
 }
 
 constexpr bool operator==(const Fraction &lhs, const Fraction &rhs)
 {
-	return lhs.num() == rhs.num() && lhs.den() == rhs.den();
+    return lhs.num() == rhs.num() && lhs.den() == rhs.den();
 }
 
 constexpr bool operator!=(const Fraction &lhs, const Fraction &rhs)
 {
-	return !(lhs == rhs);
+    return !(lhs == rhs);
 }
 
 constexpr Fraction operator*(Fraction lhs, const Fraction &rhs)
 {
-	return lhs *= rhs;
+    return lhs *= rhs;
+}
+
+constexpr Fraction operator+(Fraction lhs, const Fraction &rhs)
+{
+    return lhs += rhs;
+}
+
+constexpr Fraction operator-(Fraction lhs, const Fraction &rhs)
+{
+    return lhs -= rhs;
+}
+
+constexpr Fraction operator/(Fraction lhs, const Fraction &rhs)
+{
+    return lhs /= rhs;
 }
 
 int main()
 {
-	constexpr Fraction f1{3, 8}, f2{1, 2}, f3{10, 2};
-	std::cout << f1 << " * " << f2 << " = " << f1 * f2 << '\n'
-			  << f2 << " * " << f3 << " = " << f2 * f3 << '\n'
-			  << 2 << " * " << f1 << " = " << 2 * f1 << '\n';
-	static_assert(f3 == f2 * 10);
-}
-
-int main()
-{
-
-	return 0;
+    constexpr Fraction f1{3, 8}, f2{1, 2}, f3{10, 2};
+    std::cout << f1 << " * " << f2 << " = " << f1 * f2 << '\n'
+              << f2 << " * " << f3 << " = " << f2 * f3 << '\n'
+              << 2 << " * " << f1 << " = " << 2 * f1 << '\n';
+    static_assert(f3 == f2 * 10);
 }
--- Sem2/List6/Ex2.cpp
+++ Sem2/List6/Ex2.cpp
@@ -1,6 +1,136 @@
 #include <iostream>
+#include <cmath>
+class Fraction
+{
+    constexpr int64_t gcd(int64_t a, int64_t b) { return b == 0 ? a : gcd(b, a % b); }
+
+    int64_t n, d;
+
+public:
+    constexpr Fraction(int64_t n, int64_t d = 1) : n(n / gcd(n, d)), d(d / gcd(n, d)) {}
+
+    constexpr int64_t num() const { return n; }
+    constexpr int64_t den() const { return d; }
+
+    constexpr Fraction &operator*=(const Fraction &rhs)
+    {
+        int64_t new_n = n * rhs.n / gcd(n * rhs.n, d * rhs.d);
+        d = d * rhs.d / gcd(n * rhs.n, d * rhs.d);
+        n = new_n;
+        return *this;
+    }
+
+    constexpr Fraction &operator+=(const Fraction &rhs)
+    {
+        int64_t lcm = d * rhs.d / gcd(d, rhs.d);
+        n = n * (lcm / d) + rhs.n * (lcm / rhs.d);
+        d = lcm;
+        int64_t g = gcd(n, d);
+        n /= g;
+        d /= g;
+        return *this;
+    }
+
+    constexpr Fraction &operator-=(const Fraction &rhs)
+    {
+        int64_t lcm = d * rhs.d / gcd(d, rhs.d);
+        n = n * (lcm / d) - rhs.n * (lcm / rhs.d);
+        d = lcm;
+        int64_t g = gcd(n, d);
+        n /= g;
+        d /= g;
+        return *this;
+    }
+
+    constexpr Fraction &operator/=(const Fraction &rhs)
+    {
+        n = n * rhs.d;
+        d = d * rhs.n;
+        int64_t g = gcd(n, d);
+        n /= g;
+        d /= g;
+        return *this;
+    }
+
+    constexpr double to_double() const
+    {
+        return static_cast<double>(n) / static_cast<double>(d);
+    }
+};
+
+std::ostream &operator<<(std::ostream &out, const Fraction &f)
+{
+    return out << f.num() << '/' << f.den();
+}
+
+constexpr bool operator==(const Fraction &lhs, const Fraction &rhs)
+{
+    return lhs.num() == rhs.num() && lhs.den() == rhs.den();
+}
+
+constexpr bool operator!=(const Fraction &lhs, const Fraction &rhs)
+{
+    return !(lhs == rhs);
+}
+
+constexpr Fraction operator*(Fraction lhs, const Fraction &rhs)
+{
+    return lhs *= rhs;
+}
+
+constexpr Fraction operator+(Fraction lhs, const Fraction &rhs)
+{
+    return lhs += rhs;
+}
+
+constexpr Fraction operator-(Fraction lhs, const Fraction &rhs)
+{
+    return lhs -= rhs;
+}
+
+constexpr Fraction operator/(Fraction lhs, const Fraction &rhs)
+{
+    return lhs /= rhs;
+}
+constexpr int MAX_N_A = 100;
+constexpr int MAX_N_B = 20;
+constexpr int MAX_N_C = 15;
+
+void calculate_sum_a() {
+    std::cout << "Sum A:\n";
+    Fraction sum{0, 1};
+    for (int N = 1; N <= MAX_N_A; ++N) {
+        Fraction term{1, N * (N + 1)};
+        sum += term;
+        std::cout << "N = " << N << ", " << sum << " " << sum.to_double() << '\n';
+    }
+}
+
+void calculate_sum_b() {
+    std::cout << "\nSum B:\n";
+    Fraction sum{0, 1};
+    for (int N = 1; N <= MAX_N_B; ++N) {
+        int sign = (N % 2 == 0) ? 1 : -1;
+        Fraction term{sign, N};
+        sum += term;
+        std::cout << "N = " << N << ", " << sum << " " << sum.to_double() << '\n';
+    }
+}
+
+void calculate_sum_c() {
+    std::cout << "\nSum C:\n";
+    Fraction sum{0, 1};
+    for (int N = 1; N <= MAX_N_C; ++N) {
+        int64_t denominator = static_cast<int64_t>(pow(2, N)) * N;
+        Fraction term{1, denominator};
+        sum += term;
+        std::cout << "N = " << N << ", " << sum << " " << sum.to_double() << '\n';
+    }
+}
 
 int main() {
-
-	return 0;
+    calculate_sum_a();
+    calculate_sum_b();
+    calculate_sum_c();
+    return 0;
 }
